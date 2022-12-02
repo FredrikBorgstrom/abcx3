98a2c3af119968a66a46e0c981aa51e9dbfc259a
Fredrik Borgström <fredrik@mp1.se>
2022-12-02 09:14:38 +0100

    adding fromJson

--- libs/prisma-generator-dart/src/stubs/dart.stub.ts
+++ libs/prisma-generator-dart/src/stubs/dart.stub.ts
@@ -7,17 +7,20 @@ THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
 #{Imports}
 
 class #{ClassName} #{ParentClass} {
-#{Fields}
-  #{ClassName}({#{ConstructorArgs}});
-}
-
-
-`;
-
-export const dartConstructorArgument = `#{Required} this.#{FieldName}`;
-export const dartConstructorArgumentWithDefaultValue = `#{Required} this.#{FieldName} = #{DefaultValue}`;
-
-export const dartFieldStub = `#{Type}#{Operator} #{FieldName};`;
-
-export const dartFieldStubWithDefaultValue = `#{Type}#{Operator} #{FieldName} = #{DefaultValue};`;
+    #{Fields}
+      #{ClassName}({#{ConstructorArgs}});
+      factory #{ClassName}.fromJson(Map<String, dynamic> json) =>
+      #{ClassName}(
+        #{fromJsonArgs}
+      );
+    `;
+    
+    export const dartConstructorArgument = `#{Required} this.#{FieldName}`;
+    export const dartConstructorArgumentWithDefaultValue = `#{Required} this.#{FieldName} = #{DefaultValue}`;
+    
+    export const dartFromJsonArg = `#{FieldName}: json['#{FieldName}'] as #{Type}#{Nullable}`
+    
+    export const dartFieldStub = `#{Type}#{Operator} #{FieldName};`;
+    
+    export const dartFieldStubWithDefaultValue = `#{Type}#{Operator} #{FieldName} = #{DefaultValue};`;
 
