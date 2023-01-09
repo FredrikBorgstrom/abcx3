a522fe653c2b3a6d18895f954c7af762b7a6ab72
Fredrik Borgström <fredrik@mp1.se>
2023-01-09 20:14:46 +0100

    preparing for public release

---
+++ buildScripts/esbuild-dart.js
@@ -0,0 +1,33 @@
+const {build} = require('esbuild');
+
+const {nodeExternals} = require ('esbuild-plugin-node-externals');
+
+build({
+    entryPoints: ['./libs/prisma-generator-dart/src/generator.ts'],
+    outfile: 'built/prisma-generator-dart/prisma-generator-dart.js',
+    bundle: true,
+    minify: true,
+    platform: 'node',
+    sourcemap: false,
+    target: 'node18',
+    plugins: [nodeExternals({
+        packagePaths: ['package.json', './libs/prisma-generator-dart/package.json']
+    })]
+}).catch((err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+
+/* esbuild.build({
+    entryPoints: ['./libs/prisma-generator-dart/src/generator.ts'],
+    outfile: 'built/dart/prisma-generator-dart.js',
+    bundle: true,
+    minify: true,
+    platform: 'node',
+    sourcemap: false,
+    target: 'node18'
+}).catch((err) => {
+    console.log(err);
+    process.exit(1);
+}); */
---
+++ buildScripts/esbuild-nestjs.js
@@ -0,0 +1,37 @@
+const {build} = require('esbuild');
+
+const {nodeExternals} = require ('esbuild-plugin-node-externals');
+
+// import {build} from 'esbuild';
+
+// import {nodeExternals}  from 'esbuild-plugin-node-externals';
+
+build({
+    entryPoints: ['./libs/prisma-generator-nestjs/src/generator.ts'],
+    outfile: 'built/prisma-generator-nestjs/prisma-generator-nestjs.js',
+    bundle: true,
+    minify: true,
+    platform: 'node',
+    sourcemap: false,
+    target: 'node18',
+    plugins: [nodeExternals({
+        packagePaths: ['package.json', './libs/prisma-generator-nestjs/package.json' ]
+    })]
+}).catch((err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+
+/* esbuild.build({
+    entryPoints: ['./libs/prisma-generator-nestjs/src/generator.ts'],
+    outfile: 'built/nestjs/prisma-generator-nestjs.js',
+    bundle: true,
+    minify: true,
+    platform: 'node',
+    sourcemap: false,
+    target: 'node18'
+}).catch((err) => {
+    console.log(err);
+    process.exit(1);
+}); */
